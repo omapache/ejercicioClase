ce756e1eadcccc48866a8a3cf2108d7ac0d58fa8
omapache <owenblanco77@gmail.com>
2023-08-10 15:45:42 -0500

    feat: :zap: iniciando

---
+++ Clases/Categoria.cs
@@ -0,0 +1,18 @@
+namespace ejercicioClase.Clases;
+
+    public class Categoria
+    {
+        public string Id {get; set;}
+        public string Descripcion {get; set;}
+
+        public Categoria(string _Id, string _Descripcion){
+            this.Id = _Id
+            this.Descripcion = _Descripcion
+        }
+        public AgregarCategoria(id){
+            Console.Clear();
+            Console.WriteLine("Ingrese la Descripcion de la categoria");
+            string Descripcion = Console.ReadLine()
+            
+        }
+    }
---
+++ Clases/Producto.cs
@@ -0,0 +1,15 @@
+namespace ejercicioClase.Clases;
+
+    public class Producto
+    {        
+        public string IdCategoria {get; set;}
+        public string CodProducto {get; set;}
+        public string Nombre {get; set;}
+        public string Stock {get; set;}
+        public string StockMin {get; set;}
+        public string StockMax {get; set;}
+        public string PrecioVenta {get; set;}
+        public string PrecioCompra {get; set;}
+        public string PrecioVenta {get; set;}
+
+    }
